03b4a98832b1da3912f905a4bd0b4cd11bd0705c
vitor.curtis <vitor.curtis@b1c3f1bb-68f9-3f38-ea54-a5e2cf0566e4>
2010-06-17 13:21:15 +0000

    Versão Beta - Poucas adições

--- trunk/src/sistema/RegraReescrita.java
+++ trunk/src/sistema/RegraReescrita.java
@@ -9,7 +9,5 @@ public class RegraReescrita {
 	public RegraReescrita(String expressaoRegular, String cadeiaSubstituicao) {
 		_ER = new ExpressaoRegular(expressaoRegular);
 		_CadeiaSubstituicao = new CadeiaSustituicao(cadeiaSubstituicao, _ER._P._G._MaxIndice);
-		
-		System.out.println("Verificar se os índices de cadeia substituição existem como grupos na expressao regular");
 	}
 }
